ff87419c60b112ecc304234307a102315b44e278
henteko <henteko07@gmail.com>
2018-09-23 00:14:59 +0900

    alphaの導入

--- src/fragment_shader.js
+++ src/fragment_shader.js
@@ -1,12 +1,12 @@
 function fragmentShader() {
     return `
-  uniform vec2 uResolution;
   varying vec2 vUv;
+  varying float alpha;
   uniform sampler2D uTex;
 
   void main() {
-    vec2 st = gl_FragCoord.xy / uResolution.xy;
-    gl_FragColor = vec4(st.xy, 0.0, 0.5);
+    gl_FragColor = texture2D(uTex,vUv);
+    gl_FragColor.a = alpha;
   }
 `;
 }
--- src/vertex_shader.js
+++ src/vertex_shader.js
@@ -1,9 +1,11 @@
 function vertextShader() {
     return `
   varying vec2 vUv;
+  varying float alpha;
   
   void main() {
     vUv = uv;
+    alpha = position.y / 100.0;
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
   }
 `;
