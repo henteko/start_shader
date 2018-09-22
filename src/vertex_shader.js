48d435d0b8012e7421a9ccc1babb5a0f28742505
henteko <henteko07@gmail.com>
2018-09-23 02:03:48 +0900

    ねじれるアニメーション

--- src/vertex_shader.js
+++ src/vertex_shader.js
@@ -3,15 +3,24 @@ function vertextShader() {
   varying vec2 vUv;
   uniform float uTime;
   
-  const float power = 30.0;
-  const float speed = 13.0;
+  vec3 twist(vec3 p, float power){
+    float s = sin(power * p.y);
+    float c = cos(power * p.y);
+    mat3 m = mat3(
+      c, 0.0,  -s,
+      0.0, 1.0, 0.0,
+      s, 0.0,   c
+    );
+    return m * p;
+  }
   
   void main() {
     vUv = uv;
     
     vec3 newPos = position;
-    float gray = (newPos.y / 100.0);
-    newPos.x += sin(uTime * speed) * power * (gray * gray);
+    if(newPos.y > 45.0 && newPos.y < 100.0) {
+      newPos.xyz = twist(newPos.xyz, sin(uTime));
+    }
     
     gl_Position = projectionMatrix * modelViewMatrix * vec4( newPos, 1.0 );
   }
