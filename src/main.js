6f4942b96f3711072dad2dae9777c28181e1021c
henteko <henteko07@gmail.com>
2018-09-22 20:47:58 +0900

    各メソッドに分離した

--- src/main.js
+++ src/main.js
@@ -10,36 +10,36 @@ let scene;
 let camera;
 let renderer;
 let controls;
+let uniforms = {};
 
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 30, width / height, 1, 5000 );
     camera.position.set(126, 256, 412);
+    renderer = createRenderer(width, height);
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 50, 0 );
+    animate();
 
     const parser = new vox.Parser();
-    parser.parse("boy.vox").then(function(voxelData) {
-        const builder = new vox.MeshBuilder(voxelData, {voxelSize: 5});
-        const uniforms = {
-            uTex: {type:'t', value: builder.getTexture()},
-        };
-        const material = new THREE.ShaderMaterial({
-            transparent: true,
-            uniforms: uniforms,
-            vertexShader: vertextShader(),
-            fragmentShader: fragmentShader()
-        });
-
-        const mesh = new THREE.Mesh(builder.geometry, material);
-        scene.add( mesh );
-
-        renderer = createRenderer(width, height);
-
-        controls = new THREE.OrbitControls( camera, renderer.domElement );
-        controls.target.set( 0, 50, 0 );
-
-        animate()
-    });
+    parser.parse("boy.vox").then(addVoxel);
 }
+
+function addVoxel(voxelData) {
+    const builder = new vox.MeshBuilder(voxelData, {voxelSize: 5});
+    uniforms.uTex = {type:'t', value: builder.getTexture()};
+
+    const material = new THREE.ShaderMaterial({
+        transparent: true,
+        uniforms: uniforms,
+        vertexShader: vertextShader(),
+        fragmentShader: fragmentShader()
+    });
+
+    const mesh = new THREE.Mesh(builder.geometry, material);
+    scene.add( mesh );
+}
+
 function createRenderer(width, height) {
     const renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setClearColor( 0xa0a0a0 );
